51071d9aca5417b819a2eed3630b774e7c12a8d1
Jennifer De Araujo Oliveira <jede-ara@c1r15s11.42porto.com>
2023-11-27 17:47:46 +0000

    socorro

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: jede-ara <jede-ara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 17:25:05 by jede-ara          #+#    #+#             */
-/*   Updated: 2023/11/23 18:50:26 by jede-ara         ###   ########.fr       */
+/*   Updated: 2023/11/27 17:06:22 by jede-ara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,18 @@ class   Contact{
 
     public: 
         Contact(void); //construtor, tem que ter o mesmo nome da classe!
-        ~Contact(void); //destrutor, usado p limpeza.
+        ~Contact(void); //destrutor, usado p/ limpeza de memoria
 };
 
 //classe para o phone
 class PhoneBook{
+    private:
+        Contact contac[8];
 public:
     PhoneBook(void); //construtor
     ~PhoneBook(void); //destrutor
+    void    add(void); //funcao p add contato
+    void    search(void); //funcao p procurar contato
 };
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,21 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jede-ara <jede-ara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/23 17:17:54 by jede-ara          #+#    #+#             */
-/*   Updated: 2023/11/27 17:47:20 by jede-ara         ###   ########.fr       */
+/*   Created: 2023/11/27 17:01:42 by jede-ara          #+#    #+#             */
+/*   Updated: 2023/11/27 17:02:05 by jede-ara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
-void    PhoneBook::add(void){
-    
-}
-
-void    PhoneBook::search(void){
-    
+int main(int ac, char **av){
+ 
 }
