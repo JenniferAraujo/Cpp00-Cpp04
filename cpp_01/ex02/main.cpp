298cb0aadf072abbbb15933598f9f9a2afeeb794
Jennifer De Araujo Oliveira <jede-ara@c1r12s3.42porto.com>
2024-01-16 18:54:15 +0000

    init ex02

---
+++ cpp_01/ex02/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+#include <cctype>
+#include <iomanip>
+#include <string>
+#include <unistd.h>
+
+
+int main(void){
+    std::cout << "ola otaria" << std::endl;
+}
